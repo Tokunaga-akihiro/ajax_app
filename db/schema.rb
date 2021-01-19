bf66f5c574f9a6086e8ed29349edf0e48d4e221f
Tokunaga-akihiro <74903128+Tokunaga-akihiro@users.noreply.github.com>
2021-01-20 03:36:16 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@ ActiveRecord::Schema.define(version: 2020_10_24_171406) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  
 end
